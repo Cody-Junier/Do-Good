5db30349afad3cf3747e5a32e6e9d20e8aae6ea9
Stray <strayfrozen@yahoo.com>
2022-03-15 20:22:43 -0500

    added react-router-dom to project

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
