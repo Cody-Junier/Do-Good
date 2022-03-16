adbc1fb2af5fd186a964d7f52685fb24271b78da
Stray <strayfrozen@yahoo.com>
2022-03-15 20:29:20 -0500

    imported pages to app.js and implemented the in the jsx

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,34 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+//Imported pages
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleCharity from './pages/SingleCharity';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ApolloProvider client={client}>
+      <Router>
+        <div >
+          <Header />
+          <div >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/charity" component={SingleCharity} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
 
     </div>
   );
