04d5f1b597698ecaad2a8cd2eeb3d76e9ded687f
Greg Christiansen <gregchristiansen14@gmail.com>
2022-03-15 21:08:41 -0500

    added typeDefs, resolves, and altered the charity model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,16 +6,35 @@ const { gql } = require('apollo-server-express');
 // reactions: [Reaction]
 
 const typeDefs = gql`
-type Charity {
+  type Charity {
     _id: ID
     charityImage: Image
     createdAt: String
     username: String
-    
+    description: String
   }
-type Query {
+  type User {
+    _id: ID
+    username: String
+    email: String
     charities: [Charity]
-}
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    charities(username: String!): [Charity]
+    charity(_id: ID!): Charity
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addCharity(charityImage: Image!): Charity
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
