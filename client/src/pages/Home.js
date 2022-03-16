9960c082df7c5aa6b811f951dcae88a6ef50d97e
Stray <strayfrozen@yahoo.com>
2022-03-15 20:01:41 -0500

    added CharityList and Charity Form to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import CharityList from '../components/CharityList';
+import CharityForm from '../components/CharityForm';
+
+import Auth from '../utils/auth';
 
 
 
 const Home = () => {
+
+  const loggedIn = Auth.loggedIn();
   
 
   return (
     <main>
+      <div >
+        
+          <div >
+            <CharityForm />
+          </div>
+        
+        <div>
+         
+            <div></div>
+          
+            <CharityList
+             
+            />
+          
+        </div>
+       
+      </div>
       
     </main>
   );
