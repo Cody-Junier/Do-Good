562807fbf371e72d97f50168e7bdc654de28e32a
Stray <strayfrozen@yahoo.com>
2022-03-17 17:51:05 -0500

    added query data and commented out till querie's are complete

--- client/src/components/CharityList/index.js
+++ client/src/components/CharityList/index.js
@@ -4,22 +4,52 @@ import React from 'react';
 
 
 
-  const CharityList = ({ charity, title }) => {
-    if (!charity.length) {
-      return <h3>No Charity's Yet</h3>;
-    }
-  
+const CharityList = ({ charity, title }) => {
+  if (!charity.length) {
+    return <h3>No Charity's Yet</h3>;
+  }
+
   return (
     <section>
-    <h3>Charity List</h3>
-    <p>This is some fake charity card texdfsklsdfjsdhfkjsdfhsjkhsdjksdhksdjhsdfhsdfajlhk
-      sdhfsdfhlsdfkhadfkjdhsjdfjksdhaksdhfklhsffkdfsjkhlskjsdfhkfjhfdsksdhksdjfhfksdjshdfkj
-      sdjfsfdsdfjslsfdhksdfaklhklsdflsfkhfskfhfsdkfhfkhfdsfkhjsdfklhfsdslkhsdakjsd
-    </p>
-    
-        
       
-  </section>
+      {/* This is query data to be passed after queries are set up
+
+      <div>
+      <h3>{title}</h3>
+      {charity &&
+        charity.map(charity => (
+          <div key={charity._id} >
+            <p >
+              {charity.username}
+              thought on {charity.createdAt}
+            </p>
+            <div>
+              <p>{charity.description}</p>
+              <p>
+               
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
+    /*}
+
+
+
+
+
+      {/*Hardcoded card*/}
+      <div>
+        <h3>Charity List</h3>
+        <p>This is some fake charity card texdfsklsdfjsdhfkjsdfhsjkhsdjksdhksdjhsdfhsdfajlhk
+          sdhfsdfhlsdfkhadfkjdhsjdfjksdhaksdhfklhsffkdfsjkhlskjsdfhkfjhfdsksdhksdjfhfksdjshdfkj
+          sdjfsfdsdfjslsfdhksdfaklhklsdflsfkhfskfhfsdkfhfkhfdsfkhjsdfklhfsdslkhsdakjsd
+        </p>
+      </div>
+
+
+
+    </section>
   );
 };
 
