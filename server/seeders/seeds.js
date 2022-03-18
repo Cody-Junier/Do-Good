e6615824b0633557abce27bbca06741c188461f1
Greg Christiansen <gregchristiansen14@gmail.com>
2022-03-17 20:39:55 -0500

    updated json files along with .env plus a new seeds.js

---
+++ server/seeders/seeds.js
@@ -0,0 +1,22 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { Charity, User } = require('../models');
+
+db.once('open', async () => {
+  await Charity.deleteMany({});
+  await User.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+
+  const createdUsers = await User.collection.insertMany(userData);
+});
