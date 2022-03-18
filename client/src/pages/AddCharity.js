41acdeb431e10f07c9f9def0e00cc1efb8216ca7
cody-junier <codyjunier@gmail.com>
2022-03-17 18:15:31 -0700

    Headway on image upload

--- client/src/pages/AddCharity.js
+++ client/src/pages/AddCharity.js
@@ -1,35 +1,68 @@
-import React, {useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import {useDropzone} from 'react-dropzone'
+import { gql } from '@apollo/client'
+
 // import axios from 'axios';
 // import { gql, graphql } from 'react-apollo';
+export const ADD_CHARITY = gql`
+    mutation addCharity($charityName: String!, $charityDescription: String!, $charityUrl: String!){
+        addCharity(charityName: $charityName, charityDescription: $charityDescription, charityUrl: $charityUrl){
+            _id
+            charityName
+            charityDescription
+            charityUrl
+            createdAt
+            username
+        }
+    }
+`;
 
 const AddCharity = () => {
+    const [charityName, setCharityName] = useState('')
+    const [charityDescription, setCharityDescription] = useState('')
+    const [charityImage, setCharityImage] = useState('')
+    
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
       }, [])
-      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-    
+    const {getRootProps, getInputProps} = useDropzone({onDrop})
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            console.log({charityName, charityImage, charityDescription})
+            console.log(charityName)
+            setCharityName('')
+            console.log(charityDescription)
+            setCharityDescription('')
+            console.log(charityImage)
+            setCharityImage('')
+        } catch (e) {
+            console.log('there was an error')
+            console.error(e);
+          }
+    }
     return (
         <main>
             <h2>Hello</h2>
-        <form>
+        <form onSubmit={handleFormSubmit}>
         <input className='form-input'
                 placeholder="Charities' Name"
-                charityName='charityName'
-                type='charityName'
+                value={charityName}
                 id='charityName'
+                onChange={e => setCharityName(e.target.value)}
               />
         <textarea
           placeholder="Tell us about this Charity!"
           className="charity-description"
+          value={charityDescription}
+          id='charityDescription'
+          onChange={e => setCharityDescription(e.target.value)}
         ></textarea>
         <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+      <input {...getInputProps()} accept="image/*" onChange={e => setCharityImage(e.target.files[0])} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
     </div>
         <button >Submit</button>
         </form>
