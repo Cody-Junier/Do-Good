2eeae1955a86c9ee19202a665f3cbe73982e4a3b
byakko1022 <ravenclaw946@gmail.com>
2022-03-17 21:21:20 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
+
 
 function App() {
   return (
      <div className="App">
-    <Navbar />
+    <Navbar/>
+    <LandingPage/>
 
     </div>
   );
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,8 +3,8 @@ import logo from '../../../src/images/logo.svg';
 
 const Navbar = () => {
   return (
-    <nav className="Nav-bar" >
-    <img src={logo} />
+    <nav className="Nav-bar">
+    <img src={logo} className="App-logo"/>
     <ul className="NavList"> 
         <li><a href="" >Home</a></li>
         <li><a href="" >Log In</a></li>
