4dd43a6b8ea8367f8904bdf7fc9b1cd1e721f567
Stray <strayfrozen@yahoo.com>
2022-03-19 13:45:31 -0500

    updated the user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,9 +5,11 @@ export const QUERY_CHARITYS = gql`
   query charitys($username: String) {
     charitys(username: $username) {
       _id
+      charityName:
       createdAt
       username
       description
+      imageUrl:
     }
   }
 `;
@@ -16,9 +18,11 @@ export const QUERY_CHARITY = gql`
   query charity($id: ID!) {
     charity(_id: $id) {
       _id
+      charityName:
       createdAt
       username
       description
+      imageUrl:
     }
   }
 `;
@@ -26,9 +30,23 @@ export const QUERY_CHARITY = gql`
 
 
 export const QUERY_USER = gql`
-  
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      password:
+      charities {
+        _id
+      charityName:
+      createdAt
+      username
+      description
+      imageUrl:
+      }
+    }
+  }
 `;
-
 
 
 
