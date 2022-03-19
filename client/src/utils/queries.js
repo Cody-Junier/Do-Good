a86c9f97d46e78981d39a17a1714800d92fa4b0f
Stray <strayfrozen@yahoo.com>
2022-03-19 13:36:25 -0500

    added charity query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,9 +1,20 @@
 import { gql } from '@apollo/client';
 
 
-export const QUERY_Charitys = gql`
+export const QUERY_CHARITYS = gql`
   query charitys($username: String) {
     charitys(username: $username) {
+      _id
+      createdAt
+      username
+      description
+    }
+  }
+`;
+
+export const QUERY_CHARITY = gql`
+  query charity($id: ID!) {
+    charity(_id: $id) {
       _id
       createdAt
       username
