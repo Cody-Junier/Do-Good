703dc5ee759b9f69ee6f2110211b00537ba737da
Greg Christiansen <gregchristiansen14@gmail.com>
2022-03-20 12:21:59 -0500

    added code for the login and signup sheet but commented out along with updated the seeds.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // add authenticaion to user model
 const userSchema = new Schema(
@@ -33,7 +34,21 @@ const userSchema = new Schema(
             virtuals: true
         }
     }
-)
+);
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
