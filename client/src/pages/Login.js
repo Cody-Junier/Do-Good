fe43b363a89a2766cf2905518d9757139194c270
Greg Christiansen <gregchristiansen14@gmail.com>
2022-03-22 16:23:13 -0500

    fixed the addCharity resolver and added login/signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -34,11 +34,11 @@ const Login = (props) => {
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
+    <main>
+      <div>
+        <div>
+          <h4>Login</h4>
+          <div>
             <form onSubmit={handleFormSubmit}>
               <input
                 className='form-input'
@@ -58,7 +58,7 @@ const Login = (props) => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
+              <button type='submit'>
                 Submit
               </button>
             </form>
