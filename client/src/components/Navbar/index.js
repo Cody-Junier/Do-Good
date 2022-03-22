f16af4088fec2c776db1a7a4cbb5aea04d1d7499
byakko1022 <ravenclaw946@gmail.com>
2022-03-22 18:20:28 -0500

    fixing

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,64 +1,8 @@
 import React from 'react'
 import logo from '../../../src/images/logo.svg';
-import { Link, useNavigate } from 'react-router-dom';
-// import { AUTH_TOKEN } from '../constants';
-
-// const Navbar = () => {
-//     const navigate = useNavigate();
-//   const authToken = localStorage.getItem(AUTH_TOKEN);
-//   return ( <div className="">
-//   <div className="">
-//     <Link to="/" className="">
-//       <div className="">Hacker News</div>
-//     </Link>           
-//     <Link to="/" className="">
-//       new
-//     </Link>
-//     <div className="">|</div>
-//     <Link
-//       to="/search"
-//       className=""
-//     >
-//       search
-//     </Link>
-//     {authToken && (
-//       <div className="">
-//         <div className="">|</div>
-//         <Link
-//           to="/create"
-//           className=""
-//         >
-//           submit
-//         </Link>
-//       </div>
-//     )}
-//   </div>
-//   <div className="">
-//     {authToken ? (
-//       <div
-//         className=""
-//         onClick={() => {
-//           localStorage.removeItem(AUTH_TOKEN);
-//           navigate(`/`);
-//         }}
-//       >
-//         logout
-//       </div>
-//     ) : (
-//       <Link
-//         to="/login"
-//         className=""
-//       >
-//         login
-//       </Link>
-//     )}
-//   </div>
-// </div>
-// );
-// };
-
 
 const Navbar = () => {
+  return (
     <nav className="Nav-bar">
     <img src={logo} className="App-logo"/>
     <ul className="NavList"> 
@@ -66,6 +10,7 @@ const Navbar = () => {
         <li><a href="" >Log In</a></li>
     </ul>
  </nav>
+  )
 }
 
 export default Navbar
