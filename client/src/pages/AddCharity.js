7fac099c669ef1478bc25eaca63d24c0adc0e6f2
cody-junier <codyjunier@gmail.com>
2022-03-23 00:10:22 -0700

    trying to figure out upload + mutation

--- client/src/pages/AddCharity.js
+++ client/src/pages/AddCharity.js
@@ -3,13 +3,13 @@ import React, {useState
 } from 'react';
 // import {useDropzone} from 'react-dropzone'
 import { gql, useMutation, useQuery } from '@apollo/client'
-import Auth from '../utils/auth';
+// import Auth from '../utils/auth';
 
 import Axios from 'axios';
 // import { gql, graphql } from 'react-apollo';
 import { QUERY_CHARITIES, QUERY_ME_BASIC } from '../utils/queries';
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoiY29keS1qdW5pZXIiLCJlbWFpbCI6ImRvb3RAZ21haWwuY29tIiwiX2lkIjoiNjIzYTgzNzcwNjI5ZTg3NDJlOWFiNjk2In0sImlhdCI6MTY0ODAwNjc3OCwiZXhwIjoxNjQ4MDEzOTc4fQ.l2Ptk-3JiC5581ZyDyInwlhdoEKwPlk9syFYZV9o3HQ"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoiY29keS1qdW5pZXIiLCJlbWFpbCI6ImRvb3RAZ21haWwuY29tIiwiX2lkIjoiNjIzYTgzNzcwNjI5ZTg3NDJlOWFiNjk2In0sImlhdCI6MTY0ODAwODY4NSwiZXhwIjoxNjQ4MDE1ODg1fQ.Zc92QoWayUxdvQf2VefimiEXlnU_5g6k3Z9GH5O9-Kk"
 
 export const ADD_CHARITY = gql`
     mutation addCharity($charityName: String!, $charityDescription: String!, $charityUrl: String!, $charityImg: String!){
@@ -26,7 +26,7 @@ export const ADD_CHARITY = gql`
 `;
 
 const AddCharity = (props) => {
-    const loggedIn = Auth.loggedIn()
+    // const loggedIn = Auth.loggedIn()
 
     const [charityName, setCharityName] = useState('')
     const [charityUrl, setCharityUrl] = useState('')
@@ -38,21 +38,26 @@ const AddCharity = (props) => {
 
     const [addCharity, { error }]= useMutation(ADD_CHARITY)
 
-
+   const imageSubmit = async(event) => {
+       event.preventDefault()
+       const formData = new FormData()
+       formData.append("file", charityImage)
+       formData.append("upload_preset", "ictn1tnv")
+       
+       Axios.post(
+           `https://api.cloudinary.com/v1_1/dwgja4bfo/image/upload`,
+           formData).then((response) => {
+               console.log(response.data.url)
+               setCharityImage(response.data.url)
+               console.log(charityImage)
+            })
+        }
+            
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
-        const formData = new FormData()
-        formData.append("file", charityImage)
-        formData.append("upload_preset", "ictn1tnv")
-
-        Axios.post(
-            `https://api.cloudinary.com/v1_1/dwgja4bfo/image/upload`,
-            formData).then((response) => {
-                console.log(response.data.url)
-                setCharityImage(response.data.url)
-            })
         
+        console.log({charityName, charityUrl, charityImage, charityDescription})
         try{
             await addCharity({
                 variables: {charityName, charityUrl, charityImage, charityDescription, token}
@@ -79,7 +84,32 @@ const AddCharity = (props) => {
                     </h2> */}
               
               </div>
-          
+
+      <form onSubmit={imageSubmit}>
+            <div>
+      <h1>Upload an Image for your Charity!</h1>
+      {charityImage && (
+        <div>
+        <img alt="not found" width={"250px"} src={URL.createObjectURL(charityImage)} />
+        <br />
+        <button onClick={()=>setCharityImage(null)}>Remove</button>
+        <button>Confirm</button>
+        </div>
+      )}
+      <br />
+     
+      <br /> 
+      <input
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+        //   console.log(event.target.files[0]);
+          setCharityImage(event.target.files[0]);
+        }}
+      />
+    </div>
+    </form>
+
             <h2>Hello</h2>
         <form onSubmit={handleFormSubmit}>
         <input className='form-input'
@@ -102,27 +132,6 @@ const AddCharity = (props) => {
           onChange={e => setCharityDescription(e.target.value)}
         ></textarea>
         
-            <div>
-      <h1>Upload an Image for your Charity!</h1>
-      {charityImage && (
-        <div>
-        <img alt="not fount" width={"250px"} src={URL.createObjectURL(charityImage)} />
-        <br />
-        <button onClick={()=>setCharityImage(null)}>Remove</button>
-        </div>
-      )}
-      <br />
-     
-      <br /> 
-      <input
-        type="file"
-        name="myImage"
-        onChange={(event) => {
-        //   console.log(event.target.files[0]);
-          setCharityImage(event.target.files[0]);
-        }}
-      />
-    </div>
     {error && <span>Something went wrong...</span>}
         <button >Submit</button>
         </form>
